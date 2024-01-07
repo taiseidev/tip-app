caa44239d2a0076401e999c492966fe2e4c7f6db
onishi taisei <taisei.developer@gmail.com>
2024-01-07 22:11:43 +0900

    数字のみ入力可能なフォームのComposableを作成

--- app/src/main/java/com/example/tipapp/MainActivity.kt
+++ app/src/main/java/com/example/tipapp/MainActivity.kt
@@ -9,6 +9,7 @@ import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.wrapContentWidth
+import androidx.compose.foundation.text.KeyboardOptions
 import androidx.compose.material3.ExperimentalMaterial3Api
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Surface
@@ -21,7 +22,9 @@ import androidx.compose.runtime.remember
 import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.text.input.KeyboardType
 import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
@@ -44,10 +47,8 @@ class MainActivity : ComponentActivity() {
     }
 }
 
-@OptIn(ExperimentalMaterial3Api::class)
 @Composable
 fun TipApp(modifier: Modifier = Modifier) {
-    var amountInput by remember { mutableStateOf("0") }
     Column(
         horizontalAlignment = Alignment.CenterHorizontally, verticalArrangement = Arrangement.Center
     ) {
@@ -58,14 +59,25 @@ fun TipApp(modifier: Modifier = Modifier) {
                 .wrapContentWidth(Alignment.Start)
                 .padding(start = 64.dp)
         )
-        TextField(
-            value = amountInput,
-            onValueChange = { amountInput = it },
-            label = { /*TODO*/ },
-            modifier = Modifier.padding(16.dp)
-        )
-        Text(text = "Tip Amount: $$amountInput", fontSize = 36.sp, fontWeight = FontWeight.Bold)
+        EditNumberField()
+        Text(text = "Tip Amount: $00", fontSize = 36.sp, fontWeight = FontWeight.Bold)
     }
+}
+
+@OptIn(ExperimentalMaterial3Api::class)
+@Composable
+fun EditNumberField(modifier: Modifier = Modifier) {
+    var amountInput by remember { mutableStateOf("0") }
+    TextField(
+        value = amountInput,
+        onValueChange = { amountInput = it },
+        label = { Text(text = stringResource(id = R.string.bill_amount)) },
+        singleLine = true,
+        keyboardOptions = KeyboardOptions(
+            keyboardType = KeyboardType.Number,
+        ),
+        modifier = Modifier.padding(16.dp)
+    )
 }
 
 @Preview(showBackground = true)
